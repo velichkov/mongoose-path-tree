velichkov/mongoose-path-tree

commit 58a21f93221bc6cc2171eb795e9f22ea25a49700

Fix several eslint errors

lib/tree.js
@@ -86,12 +86,12 @@ function tree(schema, options) {
                     });
                 }
                 else {
-                    next();
+                    return next();
                 }
             });
         }
         else {
-            next();
+            return next();
         }
     });
 
@@ -106,7 +106,7 @@ function tree(schema, options) {
         if (!this.path)
             return next();
 
-        if (onDelete == 'DELETE') {
+        if (onDelete === 'DELETE') {
             this.collection.remove({ path: { '$regex': '^' + this.path + pathSeparatorRegex } }, next);
         }
         else {
@@ -132,6 +132,10 @@ function tree(schema, options) {
                     }
 
                     self.collection.find({ path: { $regex: previousParent + pathSeparatorRegex} }, function (err, cursor) {
+
+                        if (err) {
+                            return next(err);
+                        }
 
                         var subStream = cursor.stream();
                         streamWorker(subStream, numWorkers, function subStreamOnData(doc, done) {
@@ -311,8 +315,8 @@ function tree(schema, options) {
         var fields = args.fields || null;
         var options = args.options || {};
         var minLevel = args.minLevel || 1;
-        var recursive = args.recursive != undefined ? args.recursive : true;
-        var allowEmptyChildren = args.allowEmptyChildren != undefined ? args.allowEmptyChildren : true;
+        var recursive = args.recursive !== undefined ? args.recursive : true;
+        var allowEmptyChildren = args.allowEmptyChildren !== undefined ? args.allowEmptyChildren : true;
 
         if (!next)
             throw new Error('no callback defined when calling getChildrenTree');
@@ -362,7 +366,7 @@ function tree(schema, options) {
         }
         options.sort.path = 1;
 
-        if (options.lean == null) {
+        if (options.lean === null) {
             options.lean = !wrapChildrenTree;
         }
 
@@ -379,7 +383,7 @@ function tree(schema, options) {
 
             var createChildren = function createChildren(arr, node, level) {
 
-                if (level == minLevel) {
+                if (level === minLevel) {
                     if (allowEmptyChildren) {
                         node.children = [];
                     }
@@ -408,10 +412,10 @@ function tree(schema, options) {
                 minLevel = rootLevel
             }
 
-            for (var r in results) {
+            Object.keys(results).forEach(function(r) {
                 var level = getLevel(results[r].path);
                 createChildren(finalResults, results[r], level);
-            }
+            });
 
             next(err, finalResults);
 

commit 139629ceca7ec19e394ae27c0ca01ad442fbb8d5

Fix bug in getChildrenTree introduced in 58a21f9

lib/tree.js
@@ -366,7 +366,7 @@ function tree(schema, options) {
         }
         options.sort.path = 1;
 
-        if (options.lean === null) {
+        if (!options.lean) {
             options.lean = !wrapChildrenTree;
         }
 